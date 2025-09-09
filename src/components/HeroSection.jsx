96892a3ae8cbf13f43b14891e0b626e2cb2ef5f6
KashishBansal12 <kashishkb999@gmail.com>
2025-09-09 23:27:20 +0530

    updated background image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { ArrowRight } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
+import backgroundImage from '../assets/backgroundImage.png';
+
 
 
 const HeroSection = () => {
   const navigate=useNavigate();
   return (
-    <div className="flex flex-col items-start justify-center gap-4 px-4 md:px-16 lg:px-36 bg-[url('src/assets/backgroundImage.png')] bg-cover bg-center h-screen">
-        <img src={assets.marvelLogo} alt=" " className="max-h-11 lg:h-11 mt-20"/>
+ <div
+      className="flex flex-col items-start justify-center gap-4 px-4 md:px-16 lg:px-36 bg-cover bg-center h-screen"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >        <img src={assets.marvelLogo} alt=" " className="max-h-11 lg:h-11 mt-20"/>
        <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110">Guardians<br/> of the Galaxy</h1>
     <div>
         <span>Action | Adventure | Sci-Fi </span>
